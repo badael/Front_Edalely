6f4dfbb828e259351e40127fd10aa4cf5962a4cb
alaamhna3354 <alaamhna3354@gmail.com>
2021-06-30 23:25:12 +0300

    resolve i18n Translation problem

--- src/js/i18n.ts
+++ src/js/i18n.ts
@@ -1,5 +1,5 @@
 import ar from '../assets/i18n/ar.json';
-import en from '../assets/i18n/ar.json';
+import en from '../assets/i18n/en.json';
 import { createI18n } from 'vue-i18n';
 
 const i18n = createI18n({
@@ -9,6 +9,8 @@ const i18n = createI18n({
     },
     locale: localStorage.getItem('lang') || 'en',
     silentTranslationWarn: process.env.NODE_ENV === 'production',
+    fallbackLocale: 'en' // set fallback locale
+
 });
 
 export default i18n;
